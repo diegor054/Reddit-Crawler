84a21736f8b76d2cc99ee7ac88f704194a4f2b22
zdamm001 <zdamm001@ucr.edu>
2023-05-03 10:35:39 -0700

    added searching comments for links and checking robots.txt for permission to crawl

--- crawler.py
+++ crawler.py
@@ -1,5 +1,8 @@
 import praw
 import json
+from urllib.parse import urlparse
+from urllib.error import URLError
+import urllib.robotparser as urlrp
 
 #Enter your account information here
 reddit = praw.Reddit(client_id = '',
@@ -20,8 +23,11 @@ class redditPost:
     PermaLink = 'Link'
     comments = []
 
-top = reddit.subreddit("CsMajors").top(limit=100)
 checked_ids = set()
+rp = urlrp.RobotFileParser()
+
+top = reddit.subreddit("CsMajors").top(limit=13)
+
 for posts in top:
     if posts.id in checked_ids:
         continue
@@ -33,11 +39,27 @@ for posts in top:
     post.UpVotes = posts.score
     post.PostURL = posts.url
     post.PermaLink = posts.permalink
-    c = []
+    comments = []
     posts.comments.replace_more(limit=None)
     for comment in posts.comments.list():
-        c.append(comment.body)
-    post.comments = c
+        comment_obj = {"body": comment.body, "links": []}
+        for word in comment.body.split():
+            if word.startswith("http") or word.startswith("www"):
+                try:
+                    url = urlparse(word)
+                    rp.set_url(url.scheme + "://" + url.netloc + "/robots.txt")
+                    rp.read()
+                    if rp.can_fetch("*", url.geturl()):
+                        comment_obj["links"].append(word)
+                        print(f"Allowed to crawl: {word}")
+                    else:
+                        print(f"Not allowed to crawl: {word}")
+                except (URLError, UnicodeDecodeError, ValueError) as e:
+                    print(f"Invalid URL: {word}")
+                    print(f"Because {e}")
+                    continue
+        comments.append(comment.body)
+    post.comments = comments
     jsonStr = json.dumps(post.__dict__, indent=2)
 
     with open("data.json", "a") as outfile:
