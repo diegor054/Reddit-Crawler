56ff707408b57ecce91a0d90570b0cec1aad4cd6
zdamm001 <zdamm001@ucr.edu>
2023-05-05 12:07:37 -0700

    Added PostLinkTitle, made a function for get title, and each post is now on one line

--- crawler.py
+++ crawler.py
@@ -28,11 +28,40 @@ class redditPost:
     PostURL = 'URL'
     PermaLink = 'Link'
     SelfText = 'DefaultSelfText'
+    PostLinkTitle = 'DefaultTitle'
     Comments = []
-    LinkTitles = []
+    CommentLinkTitles = []
 
 checked_ids = set()
 rp = urlrp.RobotFileParser()
+
+def get_title(word):
+    try:
+        url = urlparse(word)
+        rp.set_url(url.scheme + "://" + url.netloc + "/robots.txt")
+        try:
+            rp.read()
+        except IncompleteRead as e:
+            print(f"Invalid robots.txt: {e}")
+            return None
+        if rp.can_fetch("*", url.geturl()):
+            print(f"Allowed to crawl: {word}")
+            page = requests.get(word)
+            soup = BeautifulSoup(page.content,"html.parser")
+            title = soup.title
+            if title is not None:
+                print(f"Title: {title.string}")
+                return title.string
+            else:
+                print(f"No title found for {word}")
+                return None
+        else:
+            print(f"Not allowed to crawl: {word}")
+            return None
+    except (URLError, UnicodeDecodeError, ValueError) as e:
+        print(f"Invalid URL: {word}")
+        print(f"Because {e}")
+        return None
 
 top = reddit.subreddit("suns").top(limit=1000)
 
@@ -50,40 +79,20 @@ for posts in top:
     post.PostURL = posts.url
     post.PermaLink = posts.permalink
     post.SelfText = posts.selftext
+    post.PostLinkTitle = get_title(posts.url)
     comments = []
     titles = []
     posts.comments.replace_more(limit=None)
     for comment in posts.comments.list():
         for word in comment.body.split():
             if word.startswith("http") or word.startswith("www"):
-                try:
-                    url = urlparse(word)
-                    rp.set_url(url.scheme + "://" + url.netloc + "/robots.txt")
-                    try:
-                        rp.read()
-                    except IncompleteRead as e:
-                        print(f"Invalid robots.txt: {e}")
-                        continue
-                    if rp.can_fetch("*", url.geturl()):
-                        print(f"Allowed to crawl: {word}")
-                        page = requests.get(word)
-                        soup = BeautifulSoup(page.content,"html.parser")
-                        title = soup.title
-                        if title is not None:
-                            titles.append(title.string)
-                            print(f"Title: {title.string}")
-                        else:
-                            print(f"No title found for {word}")
-                    else:
-                        print(f"Not allowed to crawl: {word}")
-                except (URLError, UnicodeDecodeError, ValueError) as e:
-                    print(f"Invalid URL: {word}")
-                    print(f"Because {e}")
-                    continue
+                title = get_title(word)
+                if title is not None:
+                    titles.append(title)
         comments.append(comment.body)
     post.Comments = comments
-    post.LinkTitles = titles
-    jsonStr = json.dumps(post.__dict__, indent=2)
+    post.CommentLinkTitles = titles
+    jsonStr = json.dumps(post.__dict__, indent=None)
 
     with open("data.json", "a") as outfile:
-        outfile.write(jsonStr)
+        outfile.write(jsonStr + '\n')
